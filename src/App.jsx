d11fc8370cb88ed0f533653f279ff0d0ccddee2b
Samuel Rafino <samucarafino@gmail.com>
2026-05-23 17:46:56 -0300

    createvite

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen">
+        <h1 className="text-3xl  ">Começando os trabalhos</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
